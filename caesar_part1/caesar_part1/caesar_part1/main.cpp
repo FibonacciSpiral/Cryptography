c16c9be25b0a1e9fb58326eb6e2f9a23b5735099
alexander-robinson-oit <alexander.robinson@oit.edu>
2020-01-12 17:30:42 -0800

    1/12/20 Initial Test Commit
    - Added project
    - main.cpp added

---
+++ caesar_part1/caesar_part1/caesar_part1/main.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <string>
+
+using std::cout;
+using std::cin;
+
+
+int main()
+{
+	cout << "Hello world!";
+}
+
+// TODO
+// Function for caesar
+// - Add for encrypt, subtract for decrypt
+// - boolean for decryption
+// - strings for file names
+// - convert key to int

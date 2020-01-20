e9308b30340f950fc5d6f2e1aa6eb3e25b717dcc
alexander-robinson-oit <alexander.robinson@oit.edu>
2020-01-19 19:27:33 -0800

    Merged branches and added caesar_part4 C++ project

---
+++ caesar_part4/caesar_part4/caesar.h
@@ -0,0 +1,22 @@
+/*
+ * caesar.h
+ * 
+ * This file will hold the declarations for functions relating to
+ * caesar deciphering
+ */
+
+#ifndef CAESAR_H
+#define CAESAR_H
+
+#define ASCII_UPPER_A 65
+#define ASCII_UPPER_Z 90
+#define ASCII_LOWER_A 97
+#define ASCII_LOWER_Z 122
+#define MAX_CHARS 2000
+
+#include <fstream>
+#include <iostream>
+
+
+
+#endif
---
+++ caesar_part4/caesar_part4/main.cpp
@@ -0,0 +1,45 @@
+/*
+ * Caesar Codebreaker
+ * 
+ * This program will take a single user input consisting of a filename
+ * for a Caesar ciphered file and result in a single most-likely
+ * translation output file.
+ */
+
+#include <iostream>
+#include <fstream>
+#include <string>
+
+using std::cout;
+using std::cin;
+using std::string;
+
+void get_input_filename(std::ifstream& file);
+
+int main()
+{
+	// first need to obtain filename input from user
+	std::ifstream input_file;
+	get_input_filename(input_file);
+	if (input_file.is_open())
+		cout << "Success" << std::endl;
+}
+
+/*
+ * get_input_filename()
+ * 
+ * Retrieves a filename from the user and attempts to open the file, 
+ * exiting the program if opening the file failed.
+ */
+void get_input_filename(std::ifstream& file)
+{
+	string filename;
+	cout << "Please enter an input filename (include file extension): ";
+	getline(cin, filename);
+	file.open(filename);
+	if (file.fail())
+	{
+		cout << "Bad file or filename. Exiting..." << std::endl;
+		exit(1);
+	}
+}
